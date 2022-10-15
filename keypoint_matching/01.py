acaa828eb42d774a8452fb2c04f2a70e0c6fcc48
Attosz <50554488+Attosz@users.noreply.github.com>
2022-10-16 00:12:22 +0200

    transform logo to target image logo place
    kinda working

--- keypoint_matching/01.py
+++ keypoint_matching/01.py
@@ -26,12 +26,11 @@ orb = cv2.ORB_create()
 keypoints1 = orb.detect(gray_img1)
 keypoints2 = orb.detect(gray_img2)
 
-
 ## kulcspont leírók számítása
 keypoints1, descriptors1 = orb.compute(gray_img1, keypoints1)
 keypoints2, descriptors2 = orb.compute(gray_img2, keypoints2)
 
-## jellemzőpontok detektálása 
+## jellemzőpontok detektálása
 surf = cv2.SIFT_create()
 keypoints1 = surf.detect(gray_img1, None)
 keypoints2 = surf.detect(gray_img2, None)
@@ -55,8 +54,36 @@ search_params = dict(checks=50)   # or pass empty dictionary
 
 flann = cv2.FlannBasedMatcher(index_params,search_params)
 
-matches = flann.knnMatch(np.asarray(descriptors1,np.float32),np.asarray(descriptors2,np.float32),k=2) ## ez most kNN-alapú, 
+matches = flann.knnMatch(descriptors1, descriptors2, k=2) ## ez most kNN-alapú,
                                                         ## minden pontnak két lehetséges párja lehet
+
+pts1 = np.array([key_point.pt for key_point in keypoints1]).reshape(-1, 1, 2)
+pts2 = np.array([key_point.pt for key_point in keypoints2]).reshape(-1, 1, 2)
+
+distances = np.array([(m1.distance, m2.distance) for (m1, m2) in matches])
+print(distances)
+
+trainIdxS = np.array([(m1.trainIdx , m2.trainIdx ) for (m1, m2) in matches])
+print(trainIdxS)
+
+asd = []
+
+# Lowe-féle arányteszt
+for i, (m, n) in enumerate(matches):
+    if m.distance < 0.7*n.distance:
+        asd.append([keypoints1[i], keypoints2[m.trainIdx]])
+
+asd2 = np.array([(kp1.pt, kp2.pt) for (kp1, kp2) in asd])
+print(asd2[:,0,:])
+
+# Elso kep illesztese a masodikra
+M, mask = cv2.findHomography(asd2[:,0,:], asd2[:,1,:], cv2.RANSAC)
+print(M)
+h, w, _ = img2.shape
+img_1to2 = cv2.warpPerspective(img1.copy(), M, (w, h))
+
+print(distances.shape)
+print(trainIdxS.shape)
 
 # Csak a jó párosítások érdekelnek bennünket, így azokat maszkoljuk
 matchesMask = [[0,0] for i in range(len(matches))]
@@ -71,7 +98,11 @@ draw_params = dict(matchColor = (0,255,0),
                    matchesMask = matchesMask,
                    flags = 0)
 
-#img3 = cv2.drawMatches(        img1,keypoints1,       img2,keypoints2,      matches[:50], None, flags=2)
-img3 = cv2.drawMatchesKnn(img1,keypoints1,img2,keypoints2,matches,None,**draw_params)
-cv2.imwrite(MATCHING_IMAGE, img3)
+# img3 = cv2.drawMatches(        img1,keypoints1,       img2,keypoints2,      matches[:50], None, flags=2)
+# img3 = cv2.drawMatchesKnn(img1, keypoints1, img2, keypoints2, matches, None)
+# cv2.imwrite(MATCHING_IMAGE, img3)
+
+cv2.imshow('asdasdasd', img2)
+cv2.imshow('result', img_1to2)
+cv2.waitKey()
 
