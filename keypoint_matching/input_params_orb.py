f62dea33a3fa759e8489af8b5b4f0613ded8ba27
Rónyai Zoltán <h773948@stud.u-szeged.hu>
2022-11-20 16:56:14 +0100

    params gui

---
+++ keypoint_matching/input_params_orb.py
@@ -0,0 +1,132 @@
+# importing libraries
+from PyQt5.QtWidgets import *
+from orb_detector import *
+import sys
+
+# creating a class
+# that inherits the QDialog class
+class Window(QDialog):
+
+    # constructor
+    def __init__(self):
+        super(Window, self).__init__()
+
+        # setting window title
+        self.setWindowTitle("Orb Detector")
+
+        # setting geometry to the window
+        self.setGeometry(100, 100, 300, 400)
+
+        # creating a group box
+        self.formGroupBox = QGroupBox("Parameters")
+
+        # creating spin box to select age
+        self.intSpinBar = QSpinBox()
+        self.doubleSpinBar = QDoubleSpinBox()
+        self.doubleSpinBar.setSingleStep(0.1)
+        # creating combo box to select degree
+        self.degreeComboBox = QComboBox()
+
+        self.mode_ransacCheckBox=QCheckBox()
+        self.draw_bounding_boxCheckBox=QCheckBox()
+
+        # adding items to the combo box
+        self.degreeComboBox.addItems(["affine", "perspective"])
+
+        # creating a line edit
+        self.nameLineEdit = QLineEdit()
+
+        # calling the method that create the form
+        self.createForm()
+
+        # creating a dialog button for ok and cancel
+        self.buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+
+        # adding action when form is accepted
+        self.buttonBox.accepted.connect(self.getInfo)
+
+        # adding action when form is rejected
+        self.buttonBox.rejected.connect(self.reject)
+
+        # creating a vertical layout
+        mainLayout = QVBoxLayout()
+
+        # adding form group box to the layout
+        mainLayout.addWidget(self.formGroupBox)
+
+        # adding button box to the layout
+        mainLayout.addWidget(self.buttonBox)
+
+        # setting lay out
+        self.setLayout(mainLayout)
+
+    # get info method called when form is accepted
+    def getInfo(self):
+
+        # printing the form information
+        print("Maxpoints : {0}".format(self.intSpinBar.text()))
+        
+        #print("Person Name : {0}".format(self.nameLineEdit.text()))
+        print("searchmode : {0}".format(self.degreeComboBox.currentText()))
+        print("Ransac : {0}".format(self.mode_ransacCheckBox.checkState()))
+        print("Bounding box : {0}".format(self.draw_bounding_boxCheckBox.checkState()))
+
+
+###############################################################################################################################
+        """
+        teszt.....
+        """
+        SRC_FOLDER = '../pic/'
+        SOURCE_IMAGE1_NAME = 'ford.png'
+        SOURCE_IMAGE2_NAME = 'ford2.jpg'
+        # SOURCE_IMAGE2_NAME = '224886291.jpg'
+        # SOURCE_IMAGE2_NAME = '255740214.jpg'
+        SOURCE_IMAGE1 = SRC_FOLDER + SOURCE_IMAGE1_NAME
+        SOURCE_IMAGE2 = SRC_FOLDER + SOURCE_IMAGE2_NAME
+        orb_detect(self.intSpinBar.text(), self.degreeComboBox.currentText(), self.mode_ransacCheckBox.checkState(), self.draw_bounding_boxCheckBox.checkState(), SOURCE_IMAGE1, SOURCE_IMAGE2)
+        
+#################################################################################################################################
+        # closing the window
+        self.close()
+
+    # creat form method
+    def createForm(self):
+        
+        # creating a form layout
+        layout = QFormLayout()
+        
+        # adding rows
+        # for name and adding input text
+        
+        layout.addRow(QLabel("maxpoints"), self.intSpinBar)
+        #layout.addRow(QLabel("str"), self.nameLineEdit)
+        
+
+        # for degree and adding combo box
+        layout.addRow(QLabel("search mode"), self.degreeComboBox)
+        layout.addRow(QLabel("MODE_RANSAC"), self.mode_ransacCheckBox)
+        layout.addRow(QLabel("DRAW_BOUNDING_BOX"), self.draw_bounding_boxCheckBox)
+
+        # for age and adding spin box
+        
+
+        # setting layout
+        self.formGroupBox.setLayout(layout)
+
+
+# main method
+if __name__ == '__main__':
+
+    
+    # create pyqt5 app
+    
+    app = QApplication(sys.argv)
+
+    # create the instance of our Window
+    window = Window()
+
+    # showing the window
+    window.show()
+
+    # start the app
+    sys.exit(app.exec())
---
+++ keypoint_matching/input_params_surf.py
@@ -0,0 +1,142 @@
+# importing libraries
+from PyQt5.QtWidgets import *
+from surf_detector import surf_detect
+import sys
+
+# creating a class
+# that inherits the QDialog class
+class Window(QDialog):
+
+    # constructor
+    def __init__(self):
+        super(Window, self).__init__()
+
+        # setting window title
+        self.setWindowTitle("Surf Detector")
+
+        # setting geometry to the window
+        self.setGeometry(100, 100, 300, 400)
+
+        # creating a group box
+        self.formGroupBox = QGroupBox("Parameters")
+
+        # creating spin box to select age
+        self.intSpinBar = QSpinBox()
+        self.doubleSpinBar = QDoubleSpinBox()
+        self.doubleSpinBar.setSingleStep(0.1)
+        # creating combo box to select degree
+        self.degreeComboBox = QComboBox()
+
+        self.mode_ransacCheckBox=QCheckBox()
+        self.draw_bounding_boxCheckBox=QCheckBox()
+
+        # adding items to the combo box
+        self.degreeComboBox.addItems(["affine", "perspective"])
+
+        # creating a line edit
+        self.nameLineEdit = QLineEdit()
+
+        # calling the method that create the form
+        self.createForm()
+
+        # creating a dialog button for ok and cancel
+        self.buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+
+        # adding action when form is accepted
+        self.buttonBox.accepted.connect(self.getInfo)
+
+        # adding action when form is rejected
+        self.buttonBox.rejected.connect(self.reject)
+
+        # creating a vertical layout
+        mainLayout = QVBoxLayout()
+
+        # adding form group box to the layout
+        mainLayout.addWidget(self.formGroupBox)
+
+        # adding button box to the layout
+        mainLayout.addWidget(self.buttonBox)
+
+        # setting lay out
+        self.setLayout(mainLayout)
+
+    # get info method called when form is accepted
+    def getInfo(self):
+
+        # printing the form information
+        print("lowe ratio : {0}".format(self.doubleSpinBar.value()))
+        
+        #print("Person Name : {0}".format(self.nameLineEdit.text()))
+        print("searchmode : {0}".format(self.degreeComboBox.currentText()))
+        print("Ransac : {0}".format(self.mode_ransacCheckBox.checkState()))
+        print("Bounding box : {0}".format(self.draw_bounding_boxCheckBox.checkState()))
+        
+###############################################################################################################################
+        """
+        teszt.....
+        """
+        SRC_FOLDER = '../pic/'
+        SOURCE_IMAGE1_NAME = 'ford.png'
+        SOURCE_IMAGE2_NAME = 'ford2.jpg'
+        # SOURCE_IMAGE2_NAME = '224886291.jpg'
+        # SOURCE_IMAGE2_NAME = '255740214.jpg'
+        SOURCE_IMAGE1 = SRC_FOLDER + SOURCE_IMAGE1_NAME
+        SOURCE_IMAGE2 = SRC_FOLDER + SOURCE_IMAGE2_NAME
+        surf_detect(self.doubleSpinBar.text(), self.degreeComboBox.currentText(), self.mode_ransacCheckBox.checkState(), self.draw_bounding_boxCheckBox.checkState(), SOURCE_IMAGE1, SOURCE_IMAGE2)
+        
+#################################################################################################################################
+        
+        """ error
+        Traceback (most recent call last):
+  File "D:\suli\haladokepfel\logodetect\keypoint_matching\input_params_surf.py", line 85, in getInfo
+    surf_detect(self.doubleSpinBar.text(), self.degreeComboBox.currentText(), self.mode_ransacCheckBox.checkState(), self.draw_bounding_boxCheckBox.checkState(), SOURCE_IMAGE1, SOURCE_IMAGE2)
+  File "D:\suli\haladokepfel\logodetect\keypoint_matching\surf_detector.py", line 188, in surf_detect
+    run_transform_finding()
+  File "D:\suli\haladokepfel\logodetect\keypoint_matching\surf_detector.py", line 130, in run_transform_finding
+    if m.distance < lowe_ratio * n.distance:
+TypeError: can't multiply sequence by non-int of type 'float'
+        """
+        
+        
+        # closing the window
+        self.close()
+
+    # creat form method
+    def createForm(self):
+        
+        # creating a form layout
+        layout = QFormLayout()
+        
+        # adding rows
+        # for name and adding input text
+        layout.addRow(QLabel("lowe ratio"), self.doubleSpinBar)
+        
+        #layout.addRow(QLabel("str"), self.nameLineEdit)
+        
+
+        # for degree and adding combo box
+        layout.addRow(QLabel("search mode"), self.degreeComboBox)
+        layout.addRow(QLabel("MODE_RANSAC"), self.mode_ransacCheckBox)
+        layout.addRow(QLabel("DRAW_BOUNDING_BOX"), self.draw_bounding_boxCheckBox)
+
+        # for age and adding spin box
+        
+
+        # setting layout
+        self.formGroupBox.setLayout(layout)
+
+
+# main method
+if __name__ == '__main__':
+
+    # create pyqt5 app
+    app = QApplication(sys.argv)
+
+    # create the instance of our Window
+    window = Window()
+
+    # showing the window
+    window.show()
+
+    # start the app
+    sys.exit(app.exec())
